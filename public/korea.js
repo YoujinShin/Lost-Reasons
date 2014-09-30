f737871e9126cc2826a9abbc9540fb4659007248
Youjin Shin <meggonagul@gmail.com>
2014-09-30 00:01:29 -0400

    add to map

--- public/korea.js
+++ public/korea.js
@@ -1,15 +1,15 @@
-var population = new L.LayerGroup(); // population layer
+// var population = new L.LayerGroup(); // population layer
 
 // populcation data
-var rateById = d3.map();
+// var rateById = d3.map();
 
-// var quantize = d3.scale.quantize()
-// 	.domain([0, 9000])
-// 	.range(d3.range(9).map(function(i) { return "q"+i+"-9"; }));
+// // var quantize = d3.scale.quantize()
+// // 	.domain([0, 9000])
+// // 	.range(d3.range(9).map(function(i) { return "q"+i+"-9"; }));
 
 
 queue() // upload data using queue 
-	.defer(d3.json, "tracts_2010.geojson") // cambridge GIS data
+	.defer(d3.json, "skorea-municipalities-topo.json") // cambridge GIS data
 	// .defer(d3.csv, "camb_tract_pop_2010.csv", function(d) { 
 	// 	rateById.set(d.id, +d.population); // cambridge CDD data
 	// 	popAcrebyID.set(d.id, + d.population_per_acre);
@@ -18,19 +18,22 @@ queue() // upload data using queue
 	.await(ready);
  
 
-function ready(error, tract) {
-	console.log("tract geographic data uploaded");
+function ready(error, kor) {
+	console.log("geographic data uploaded");
 
-	L.geoJson(tract, {
+	var municipalities = topojson.object(kor, kor.objects['skorea-municipalities-geo']);
+	var geometries = municipalities.geometries;
+	// console.log(geometries);
+
+	L.geoJson(geometries, {
 		style: function(feature) {
-			var tract = feature.properties.NAME10;
-			var c = getColor( quantize( rateById.get(tract) ) );
+			// var tract = feature.properties.NAME10;
+			// var c = getColor( quantize( rateById.get(tract) ) );
 
 			return {
-				color: c,
+				color: 'blue',
 				weight: 1.3,
-				fillColor: c,
-				fillOpacity: 0.84
+				fillOpacity: 0
 			};
 		},
 
@@ -45,8 +48,8 @@ var grayscale   = L.tileLayer(mbUrl, {id: 'examples.map-20v6611k'}),
 	streets  = L.tileLayer(mbUrl, {id: 'examples.map-i875mjb7'});
 
 var map = L.map('map', {
-	center: [42.3783903,-71.1129096],
-	zoom: 13,
+	center: [35.8615204,127.096405],
+	zoom: 7,
 	layers: grayscale
 });
 
